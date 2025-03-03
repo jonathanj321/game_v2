3b63ceb5867e633c0a4d9f4b645d0390390cc32e
morris-francis-collins <morris.francis.collins@gmail.com>
2025-03-03 01:32:33 -0800

    first changes

--- game_v2/GameViewController.swift
+++ game_v2/GameViewController.swift
@@ -7,38 +7,66 @@
 
 import UIKit
 import SpriteKit
-import GameplayKit
+import SwiftUI
 
 class GameViewController: UIViewController {
-
+    
+    // Create joystick state objects
+    let leftJoystickState = JoystickState()
+    let rightJoystickState = JoystickState()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+        // Set up the SpriteKit view
+        let skView = SKView(frame: view.bounds)
+        view.addSubview(skView)
+        
+        let scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .aspectFill
+        skView.presentScene(scene)
+        skView.ignoresSiblingOrder = true
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        
+        // Overlay the joystick views
+        setupJoystickViews()
     }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+    
+    func setupJoystickViews() {
+        let joystickSize: CGFloat = 150
+        
+        // Left joystick (bottom left)
+        let leftJoystickVC = UIHostingController(
+            rootView: JoystickView(joystickState: leftJoystickState)
+        )
+        leftJoystickVC.view.frame = CGRect(
+            x: 20,
+            y: view.bounds.height - joystickSize - 20,
+            width: joystickSize,
+            height: joystickSize
+        )
+        leftJoystickVC.view.backgroundColor = .clear
+        addChild(leftJoystickVC)
+        view.addSubview(leftJoystickVC.view)
+        leftJoystickVC.didMove(toParent: self)
+        
+        // Right joystick (bottom right)
+        let rightJoystickVC = UIHostingController(
+            rootView: JoystickView(joystickState: rightJoystickState)
+        )
+        rightJoystickVC.view.frame = CGRect(
+            x: view.bounds.width - joystickSize - 20,
+            y: view.bounds.height - joystickSize - 20,
+            width: joystickSize,
+            height: joystickSize
+        )
+        rightJoystickVC.view.backgroundColor = .clear
+        addChild(rightJoystickVC)
+        view.addSubview(rightJoystickVC.view)
+        rightJoystickVC.didMove(toParent: self)
     }
-
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
